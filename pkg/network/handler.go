56bbac0f1c2d69c4bae55a177e99ab703a3cc73e
mathiaas@live.no <mathiaas@live.no>
2024-11-22 07:55:43 +0100

    wip packet handler

--- pkg/network/handler.go
+++ pkg/network/handler.go
@@ -1,24 +1,46 @@
 package network
 
 import (
-	"net"
 	"fmt"
+	"net"
+
 	"github.com/mathiascn/multiplayer_server/pkg/protocol"
+	"github.com/mathiascn/multiplayer_server/pkg/version"
+	"github.com/mathiascn/multiplayer_server/pkg/protocol/messages"
 )
 
+func HandleHandshake(message []byte) {
+	//deserialize incoming handshake
+	handshake, err := messages.DeserializeHandshakePayload([]byte(message))
+	if err != nil {
+		fmt.Println("Error deserializing handshake", err)
+	}
+
+	fmt.Printf("Client handshake: Version %d.%d.%d\n", handshake.Major, handshake.Minor, handshake.Patch)
+	//conditional response
+	if !version.IsClientCompatible(handshake.Major, handshake.Minor, handshake.Patch) {
+		response = ""
+	}
+
+	newPacket := protocol.EncodePacket(protocol.MessageTypeHandshake, response)
+	_, err = conn.WriteToUDP(newPacket, addr)
+	if err != nil {
+		fmt.Println("Error sending response to client:", err)
+	}
+}
+
 func HandlePacket(conn *net.UDPConn,addr *net.UDPAddr, packet []byte) {
-	_, message, err := protocol.DecodePacket(packet) // Use message type
+	messageType, message, err := protocol.DecodePacket(packet) // Use message type
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	fmt.Printf("Received from %v: %s\n", addr, message)
 
-	response := fmt.Sprintf("Pong: %s", message)
-	newPacket := protocol.EncodePacket(protocol.MessageTypePong, response)
-
-	_, err = conn.WriteToUDP(newPacket, addr)
-	if err != nil {
-		fmt.Println("Error sending response to client:", err)
+	switch messageType {
+	case protocol.MessageTypeHandshake:
+		HandleHandshake()
+	default:
+		fmt.Println("Unknown message type %d\n", messageType)
 	}
 }
